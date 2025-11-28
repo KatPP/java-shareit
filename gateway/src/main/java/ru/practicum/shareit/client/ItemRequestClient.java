868407544b5b49508574e013c9c9056eb6adc4de
zenit1925 <kostia-85-85@yandex.ru>
2025-11-28 14:22:49 +0300

    Реализация Проект ShareIt. Часть 3 - fix12

--- gateway/src/main/java/ru/practicum/shareit/client/ItemRequestClient.java
+++ gateway/src/main/java/ru/practicum/shareit/client/ItemRequestClient.java
@@ -1,10 +1,7 @@
 package ru.practicum.shareit.client;
 
-import org.apache.hc.client5.http.classic.HttpClient;
-import org.apache.hc.client5.http.impl.classic.HttpClients;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.http.ResponseEntity;
-import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.web.util.DefaultUriBuilderFactory;
 
@@ -17,11 +14,9 @@ public class ItemRequestClient extends BaseClient {
     }
 
     private org.springframework.web.client.RestTemplate getRestTemplate() {
-        var httpClient = HttpClients.createDefault();
-        var requestFactory = new HttpComponentsClientHttpRequestFactory((HttpClient) httpClient);
         return builder
                 .uriTemplateHandler(new DefaultUriBuilderFactory(serverUrl))
-                .requestFactory(() -> requestFactory)
+                .requestFactory(org.springframework.http.client.SimpleClientHttpRequestFactory::new)
                 .build();
     }
 
